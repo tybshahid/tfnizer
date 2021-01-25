4acaaed68aab50f329914fdd1a3616d5f5084976
Tayyab Shahid <tayyab.shahid@openoffice.com.au>
2021-01-25 21:00:36 +1100

    Disabled UseHttpsRedirection

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
